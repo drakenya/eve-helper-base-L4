018fb7828d4cc6473a757f44e077def9adcc7eba
Andrew Kroll <krolla@gmail.com>
2013-11-12 18:25:50 -0500

    Initial push

---
+++ manifests/default.pp
@@ -0,0 +1,146 @@
+group { 'puppet': ensure => present }
+Exec { path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ] }
+File { owner => 0, group => 0, mode => 0644 }
+
+class {'apt':
+  always_apt_update => true,
+}
+
+Class['::apt::update'] -> Package <|
+    title != 'python-software-properties'
+and title != 'software-properties-common'
+|>
+
+    apt::key { '4F4EA0AAE5267A6C': }
+
+apt::ppa { 'ppa:ondrej/php5-oldstable':
+  require => Apt::Key['4F4EA0AAE5267A6C']
+}
+
+class { 'puphpet::dotfiles': }
+
+package { [
+    'build-essential',
+    'vim',
+    'curl',
+    'git-core'
+  ]:
+  ensure  => 'installed',
+}
+
+class { 'apache': }
+
+apache::dotconf { 'custom':
+  content => 'EnableSendfile Off',
+}
+
+apache::module { 'rewrite': }
+
+apache::vhost { 'eve-helper.dev':
+  server_name   => 'eve-helper.dev',
+  serveraliases => [
+],
+  docroot       => '/var/www/www/public/',
+  port          => '80',
+  env_variables => [
+],
+  priority      => '1',
+}
+apache::vhost { 'docs.eve-helper.dev':
+  server_name   => 'docs.eve-helper.dev',
+  serveraliases => [
+],
+  docroot       => '/var/www/',
+  port          => '80',
+  env_variables => [
+],
+  priority      => '1',
+}
+
+class { 'php':
+  service             => 'apache',
+  service_autorestart => false,
+  module_prefix       => '',
+}
+
+php::module { 'php5-pgsql': }
+php::module { 'php5-cli': }
+php::module { 'php5-curl': }
+php::module { 'php5-intl': }
+php::module { 'php5-mcrypt': }
+
+class { 'php::devel':
+  require => Class['php'],
+}
+
+class { 'php::pear':
+  require => Class['php'],
+}
+
+
+
+
+
+class { 'xdebug':
+  service => 'apache',
+}
+
+class { 'composer':
+  require => Package['php5', 'curl'],
+}
+
+puphpet::ini { 'xdebug':
+  value   => [
+    'xdebug.default_enable = 1',
+    'xdebug.remote_autostart = 0',
+    'xdebug.remote_connect_back = 1',
+    'xdebug.remote_enable = 1',
+    'xdebug.remote_handler = "dbgp"',
+    'xdebug.remote_port = 9000'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_xdebug.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+puphpet::ini { 'php':
+  value   => [
+    'date.timezone = "America/New_York"'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_php.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+puphpet::ini { 'custom':
+  value   => [
+    'display_errors = On',
+    'error_reporting = -1'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_custom.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+
+class { 'postgresql':
+  charset => 'UTF8',
+  locale  => 'en_US.UTF-8',
+}->
+class { 'postgresql::server':
+  config_hash => {
+    postgres_password => 'password',
+  },
+}
+
+postgresql::db { 'eve_helper':
+  user     => 'eve_helper',
+  password => 'eve_helper',
+  grant    => 'ALL',
+}
+postgresql::db { 'eve_data':
+  user     => 'eve_data',
+  password => 'eve_data',
+  grant    => 'ALL',
+}
+
